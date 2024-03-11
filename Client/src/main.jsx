d26b6f98c6305b9d778f459e1ed6ec2a4859ce8e
satwiktiwari22 <satwiktiwari210196@acropolis.in>
2024-03-12 00:06:43 +0530

    Add react-router-dom and update routes

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import StudentDashboard from "./Pages/StudentDashboard.jsx";
 
 const theme = createTheme({
   palette: {
@@ -15,7 +17,12 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/student" element={<StudentDashboard />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   </React.StrictMode>
 );
