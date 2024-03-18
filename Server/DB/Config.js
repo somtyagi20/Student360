8c8fd42a44d13ad4b7629d6cc3ad024184a282ca
somtyagi20 <somnathtyagi210787@acropolis.in>
2024-03-18 12:18:03 +0530

    Add prettier config

--- Server/DB/Config.js
+++ Server/DB/Config.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 
-const db = mongoose
-  .connect("mongodb://localhost:27017/SRAS", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connected");
-  })
-  .catch((err) => {
-    console.log("DB Connection Error: " + err);
-  });
+const db = () => {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/SRAS")
+    .then(() => {
+      console.log("DB Connected");
+    })
+    .catch((err) => {
+      console.log("DB Connection Error: " + err);
+    });
+};
 
 export default db;
--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import connectDB from "./DB/Config.js";
 
 const app = express();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
