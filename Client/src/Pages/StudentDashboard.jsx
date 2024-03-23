66ac25ad68bde101553d4105970c4f03ea75983c
shailendra parihar <shailendraparihar210156@acropolis.in>
2024-03-23 11:17:10 +0530

    student dashboard header and image implemented

--- Client/src/Pages/StudentDashboard.jsx
+++ Client/src/Pages/StudentDashboard.jsx
@@ -1,7 +1,27 @@
 import "./StudentDashboard.css";
+import image from "../assets/Avatar 5.png";
 
+const user = {
+  name: "Active Panda",
+  class: "CS-4",
+  img: image,
+}
 const StudentDashboard = () => {
-  return <div className="dashboard-container"></div>;
-};
+  return (
+    <div className="Student-header">
+    <img src={user.img} alt="" className="user-image"/>
+    <div className="st-background"></div>
+    <div className="st-details">
+    <div className="details">
+    <b>Name: </b>
+    <span>{user.name}</span>
+    <br />
+    <b>Class: </b>
+    <span>{user.class}</span>
+    </div>
+    </div>
+    </div>
+  );
+}; 
+export  default  StudentDashboard;
 
-export default StudentDashboard;
