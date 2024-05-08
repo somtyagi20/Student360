2407b9b7bc1415876260efb6c9daa827ce7eb6b1
somtyagi20 <somnathtyagi210787@acropolis.in>
2024-05-08 22:56:06 +0530

    chore: Update CORS configuration and middleware order in app.js

--- Server/app.js
+++ Server/app.js
@@ -7,10 +7,10 @@ import adminRoutes from "./Routes/adminRoutes.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
-app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/v1/student", userRoutes);
