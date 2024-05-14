4a2097c4d19849c306c4aaf41114ba08087f4e08
satwiktiwari22 <satwiktiwari210196@acropolis.in>
2024-05-14 23:29:10 +0530

    chore: Update CSS and component structure in FacultyDashboard.jsx and remove unused StudentDetail component

--- Client/src/Components/FacultyInfoCard.jsx
+++ Client/src/Components/FacultyInfoCard.jsx
@@ -1,24 +1,143 @@
-import React from 'react';
-import { Card, CardContent, CardMedia, Typography, Box, Stack } from '@mui/material';
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Box,
+  Stack,
+} from "@mui/material";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import { useState } from "react";
+import { IconButton } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import UpdateFaculty from "./UpdateFaculty";
 
-
-const FacultyInfoCard = ({FacultyName="Default",assignedClass="Default"}) => {
+const FacultyInfoCard = () => {
+  const { user, setUser } = useContext(UserContext);
+  const [formVisible, setFormVisible] = useState(false);
   return (
     <>
-      <Card sx={{ borderRadius: '5px', backgroundColor: '#F3F4F6FF' }}>
+      <Card
+        sx={{
+          borderRadius: "5px",
+          background: "linear-gradient(to right, #f5f5f5, #cdacff, #6716e1)",
+          boxShadow: "0 0 8px 0 #000",
+          padding: "10px",
+        }}
+      >
         {/* Use a container for content within the card with appropriate spacing */}
-        <CardContent sx={{ p: 2, display: 'flex', justifyContent: 'center' }}>
+        <CardContent
+          sx={{
+            p: 2,
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
           {/* Use Stack for vertical alignment and horizontal centering */}
-          <Stack direction="column" spacing={2}>
-            <Typography variant="h5" sx={{ fontFamily: 'Lexend', fontSize: '24px', fontWeight: 700, color: '#171A1FFF', textAlign: 'center' }}>
-              Faculty Name - {FacultyName}
+          {/* <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "130px",
+              height: "130px",
+              borderRadius: "50%",
+              overflow: "hidden",
+              boxShadow: "0 0 8px 0 #000",
+            }}
+          >
+            <img src={user && user.profile_pic} alt="no" />
+          </div> */}
+          <Stack direction="column" spacing={1.5}>
+            <Typography
+              variant="h5"
+              sx={{
+                fontSize: "22px",
+                fontWeight: 700,
+                color: "#171A1FFF",
+                textAlign: "center",
+              }}
+            >
+              <span
+                style={{
+                  color: "#6716e1",
+                }}
+              >
+                Faculty Name
+              </span>{" "}
+              - {user && user.name}
             </Typography>
-            <Typography variant="h5" sx={{ fontFamily: 'Lexend', fontSize: '24px', fontWeight: 700, color: '#171A1FFF', textAlign: 'center' }}>
-              Assigned Class - {assignedClass}
+            <Typography
+              variant="h5"
+              sx={{
+                fontSize: "22px",
+                fontWeight: 700,
+                color: "#171A1FFF",
+                textAlign: "center",
+              }}
+            >
+              <span
+                style={{
+                  color: "#6716e1",
+                }}
+              >
+                Department
+              </span>{" "}
+              - {user && user.department}
+            </Typography>
+
+            <Typography
+              variant="h5"
+              sx={{
+                fontSize: "22px",
+                fontWeight: 700,
+                color: "#171A1FFF",
+                textAlign: "center",
+              }}
+            >
+              <span
+                style={{
+                  color: "#6716e1",
+                }}
+              >
+                Assigned Class
+              </span>{" "}
+              -{" "}
+              {user && user.accessed_class?.length > 0
+                ? user.accessed_class
+                : "No Class Assigned"}
             </Typography>
           </Stack>
         </CardContent>
       </Card>
+      <IconButton
+        sx={{
+          position: "fixed",
+          top: "70px",
+          right: "10px",
+          color: "black",
+
+          background: "#f5f5f5",
+          borderRadius: "50%",
+
+          ":hover": {
+            color: "white",
+            background: "#171A1FFF",
+            boxShadow: "0 0 4px 0 #000",
+          },
+
+          zIndex: 1,
+        }}
+        onClick={() => {
+          setFormVisible(true);
+        }}
+      >
+        <EditIcon />
+      </IconButton>
+      {formVisible && <UpdateFaculty setFormVisible={setFormVisible} />}
     </>
   );
 };
--- Client/src/Components/StudentTable.jsx
+++ Client/src/Components/StudentTable.jsx
@@ -1,62 +1,255 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  InputAdornment,
+  TextField,
+  IconButton,
+  Typography,
+  Box,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import DownloadIcon from "@mui/icons-material/Download";
+import AddIcon from "@mui/icons-material/Add";
 
-function createData(EnrollmentNo, StudentName, Class) {
-  return { EnrollmentNo, StudentName, Class};
-}
+// function createData(EnrollmentNo, StudentName, Class) {
+//   return { EnrollmentNo, StudentName, Class };
+// }
 
-const rows = [
-  createData('Student Name 1', 159, 6.0),
-  
-];
+// const rows = [
+//   {
+//     EnrollmentNo: "123456",
+//     StudentName: "John Doe",
+//     Class: "10th",
+//   },
+//   {
+//     EnrollmentNo: "123457",
+//     StudentName: "Jane Doe",
+//     Class: "10th",
+//   },
+// ];
 
-export default function StudentTable() {
-  return (
-    <TableContainer component={Paper} sx={{
-      marginTop: 5,
+export default function StudentTable({ Class }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleDownload = (event) => {
+    // Handle download functionality
+  };
+
+  const handleAddmarks = (event) => {
+    // Handle add marks functionality
+  };
+  const [rows, setRows] = useState([]);
+
+  const getStudent = async (id) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/v1/faculty/getstudent?id=${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      console.log(response.data.data);
+    } catch (error) {
+      console.error(error);
     }
-    }>
-      <Table sx={{width:'100%' }} aria-label="simple table">
-        <TableHead>
-          <TableRow
+  };
+
+  const getStudents = async (Class) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/v1/faculty/studentsbyclass?class=${Class}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      console.log(response.data.data);
+      setRows(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSearch = (event) => {
+    const term = event.target.value;
+    setSearchTerm(term);
+    if (term === "") {
+      getStudents(Class);
+      setRows(rows);
+    } else {
+      setRows(
+        rows.filter((row) =>
+          row.name.toLowerCase().includes(term.toLowerCase())
+        )
+      );
+    }
+  };
+
+  useEffect(() => {
+    getStudents(Class);
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        marginLeft: 4,
+      }}
+    >
+      <Typography variant="body1" color="black" fontSize={30} fontWeight={500}>
+        Student Details
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "100%",
+          marginTop: 3,
+        }}
+      >
+        <TextField
+          id="outlined-start-adornment"
           sx={{
-            backgroundColor: 'grey',
-          }}>
-            <TableCell style={{ fontWeight: '650' }}>Enrollment Number</TableCell>
-<TableCell align="right" style={{ fontWeight: '650' }}>Student Name</TableCell>
-<TableCell align="right" style={{ fontWeight: '650' }}>Class</TableCell>
-            </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.EnrollmentNo}
+            width: { xs: "100%", sm: "400px" },
+            height: "30px",
+            marginRight: 2,
+          }}
+          value={searchTerm}
+          onChange={handleSearch}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <IconButton
+                  sx={{
+                    "&:hover": {
+                      color: "#171A1FFF",
+                    },
+                  }}
+                >
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+            placeholder: "Search...",
+          }}
+        />
+
+        <Box sx={{ display: "flex", marginRight: 4 }}>
+          <Button
+            variant="contained"
+            sx={{
+              border: 0,
+              marginLeft: 2,
+              backgroundColor: "inherit",
+              height: "36px",
+              color: "#171A1FFF",
+              "&:hover": {
+                backgroundColor: "#2ba3fc",
+                color: "#FFFFFF",
+              },
+            }}
+            onClick={handleDownload}
+            startIcon={<DownloadIcon />}
+          >
+            Download Student List
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              border: 0,
+              marginLeft: 2,
+              backgroundColor: "inherit",
+              height: "36px",
+              color: "#171A1FFF",
+              "&:hover": {
+                backgroundColor: "#2ba3fc",
+                color: "#FFFFFF",
+              },
+            }}
+            onClick={handleAddmarks}
+            startIcon={<AddIcon />}
+          >
+            Add marks
+          </Button>
+        </Box>
+      </Box>
+
+      <TableContainer
+        component={Paper}
+        sx={{
+          marginTop: 5,
+        }}
+      >
+        <Table sx={{ width: "100%" }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: "650" }}>
+                Enrollment Number
               </TableCell>
-              <TableCell align="right">{row.StudentName}</TableCell>
-              <TableCell align="right">{row.Class}</TableCell>
-              
+              <TableCell align="right" sx={{ fontWeight: "650" }}>
+                Student Name
+              </TableCell>
+              <TableCell align="right" sx={{ fontWeight: "650" }}>
+                Class
+              </TableCell>
+              <TableCell align="right" sx={{ fontWeight: "650" }}>
+                Click to view details
+              </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows &&
+              rows
+                .sort((a, b) => a.name.localeCompare(b.name))
+                .map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell align="left">{row.enrollment_no}</TableCell>
+                    <TableCell align="right">{row.name}</TableCell>
+                    <TableCell align="right">{row.class}</TableCell>
+                    <TableCell align="right">
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => {
+                          console.log(row);
+                          getStudent(row._id);
+                        }}
+                      >
+                        View
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
-          }
+}
 //   ** Use this code while integrating backend **
 
 // export default function StudentTable() {
 //     const [rows, setRows] = useState([]);
-  
+
 //     useEffect(() => {
 //       // Fetch data from backend API
 //       fetch('your-backend-api-endpoint') // Replace 'your-backend-api-endpoint' with your actual backend API endpoint
@@ -69,7 +262,7 @@ export default function StudentTable() {
 //           console.error('Error fetching data:', error);
 //         });
 //     }, []); // Empty dependency array ensures this effect runs only once, similar to componentDidMount
-  
+
 //     return (
 //       <TableContainer component={Paper}>
 //         <Table sx={{ minWidth: 650 }} aria-label="simple table">
